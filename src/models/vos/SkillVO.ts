c0f95284d2cc25a81f57a79a463f8501eefa7388
Collin Howland <collin.howland@hotmail.com>
2020-08-06 18:49:39 -0700

    created SkillVO

---
+++ src/models/vos/SkillVO.ts
@@ -0,0 +1,8 @@
+export class SkillVO {
+    name: string = ''
+    rank: number = 0
+    maxRank: number = 0
+    description: string = ''
+    currentRankEffect?: string
+    nextRankEffect?: string
+}
